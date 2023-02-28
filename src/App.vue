a5a23bd225372e1561d3ae7dba3b987a228295ba
huangdongjiang <dongjiang.huang@get88.cn>
2023-02-28 18:25:39 +0800

    chore: unplugin-component-vite config

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts"></script>
 <template>
-  <router-view></router-view>
+  <BaseLayout>
+    <RouterView />
+  </BaseLayout>
 </template>
 
 <style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
-
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -12,6 +13,18 @@ export default defineConfig({
       imports: ['vue'],
       dts: './auto-imports.d.ts',
       vueTemplate: true,
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      extensions: ['vue', 'md', 'svg'],
+      // directoryAsNamespace: true,
+      include: [/\.vue$/, /\.md$/],
+      dts: true,
+      resolvers: [ElementPlusResolver({})],
+      dirs: [
+        path.resolve(__dirname, './src/layout'),
+        path.resolve(__dirname, './src/components'),
+      ],
     }),
   ],
   resolve: {
